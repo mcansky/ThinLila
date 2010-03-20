2e29fb41d38be5afc66aae8175debf31696c9783
T Riboulet <riboulet@gmail.com>
2010-03-20 22:42:01 +0100

    adding cucumber basic feature and steps

---
+++ features/loading.feature
@@ -0,0 +1,12 @@
+Feature: Load a server config from a yaml file
+	In order to load a server config
+	I want to get the vars needed
+
+Scenario: A yaml file
+	Given the file config/config.yml
+	When loading the file
+	Then the server should have a name
+	And the server should have an ip address
+	And the server should have a port
+	And the server should have a path
+	And the server should have servers
---
+++ features/step_definitions/loading_steps.rb
@@ -0,0 +1,27 @@
+Given /^the file config\/config\.yml$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^loading the file$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the server should have a name$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the server should have an ip address$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the server should have a port$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the server should have a path$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the server should have servers$/ do
+  pending # express the regexp above with the code you wish you had
+end
